fbdebfb9c86ae27708b56e73329b66db37d31fd7
h-east <h.east.727@gmail.com>
2025-02-16 02:21:41 +0900

    first commit

--- src/term.c
+++ src/term.c
@@ -2956,6 +2956,15 @@ out_str_cf(char_u *s)
     void
 out_str(char_u *s)
 {
+    if (s == T_CL)
+    {
+	HH_ch_log("XXX T_CL");
+	if (g_do_ex_pwd)
+	{
+	    HH_ch_log("XXX g_do_ex_pwd:%d", g_do_ex_pwd);
+	    return;
+	}
+    }
     if (s == NULL || *s == NUL)
 	return;
 
@@ -3993,12 +4002,14 @@ starttermcap(void)
     void
 stoptermcap(void)
 {
+    HH_ch_log("in. termcap_active:%d", termcap_active);
     screen_stop_highlight();
     reset_cterm_colors();
 
     if (!termcap_active)
 	return;
 
+    HH_ch_log("gui.in_use:%d, gui.starting:%d", gui.in_use, gui.starting);
 #ifdef FEAT_TERMRESPONSE
 # ifdef FEAT_GUI
     if (!gui.in_use && !gui.starting)
@@ -4007,6 +4018,7 @@ stoptermcap(void)
 	// May need to discard T_CRV, T_U7 or T_RBG response.
 	if (termrequest_any_pending())
 	{
+	    HH_ch_log("termrequest_any_pending is TRUE");
 # ifdef UNIX
 	    // Give the terminal a chance to respond.
 	    mch_delay(100L, 0);
@@ -4053,6 +4065,7 @@ stoptermcap(void)
 					// Kitty keyboard protocol
     screen_start();			// don't know where cursor is now
     out_flush();
+    HH_ch_log("out.");
 }
 
 #if defined(FEAT_TERMRESPONSE) || defined(PROTO)
